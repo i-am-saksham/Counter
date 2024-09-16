507dd3712f0dc41769e3bd575a3c477bc4bdeb02
Saksham Bhardwaj <125785732+i-am-saksham@users.noreply.github.com>
2024-09-16 09:45:19 +0530

    Update App.jsx

--- App.jsx
+++ App.jsx
@@ -1,17 +1,10 @@
 import { useState } from 'react'/*for hooks */
-// import reactLogo from './assets/react.svg'
-// import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  // const [count, setCount] = useState(0)
-  // useState hook has two oarameter. first is the variable name and second is the function name.
-
   let  [counter, setCounter] = useState(10)
 
-    // let counter = 15;
     const addValue = () =>{
-      // console.log("Clicked",counter);  
       counter = counter + 1;
        
       
